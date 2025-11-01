0100fb45f40cf051104698b5756c89143914b315
Francisco Agustin García <franciscoagustingarciagarcia@gmail.com>
2025-11-01 20:20:53 -0300

    Agrego actualizar pedido

--- Database/Pedido/sp_pedido.sql
+++ Database/Pedido/sp_pedido.sql
@@ -10,4 +10,18 @@ BEGIN
 	VALUES (xNombre, xPeso, xVolumen, xEstado, xOrigen, xDestino, xidVehiculo, xidAdministrador, xidEmpresa);
 	set xidPedido = last_insert_id();
 END $$
+DELIMITER $$
 
+/*-----------------------------------------------*/
+-- Store Procedures ACTUALIZAR Tabla Pedido
+/*-----------------------------------------------*/
+
+DELIMITER $$
+DROP procedure IF EXISTS sp_Pedido_actualizar $$
+CREATE PROCEDURE sp_Pedido_actualizar (IN xidPedido INT, IN xEstado Varchar(45))
+BEGIN
+	UPDATE Pedido 
+	SET xEstado = xEstado
+	WHERE xidPedido = xidPedido;
+END $$
+DELIMITER $$
