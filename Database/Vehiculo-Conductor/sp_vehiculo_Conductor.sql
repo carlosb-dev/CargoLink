adbbdabc0f46dac8962ed8e2500bd1682161f9e1
Francisco Agustin García <franciscoagustingarciagarcia@gmail.com>
2025-11-01 18:11:42 -0300

    Update sp_vehiculo_Conductor.sql

--- Database/Vehiculo-Conductor/sp_vehiculo_Conductor.sql
+++ Database/Vehiculo-Conductor/sp_vehiculo_Conductor.sql
@@ -6,8 +6,26 @@ DELIMITER $$
 DROP procedure IF EXISTS sp_Vehiculo_Conductor_insertar $$
 CREATE PROCEDURE sp_Vehiculo_Conductor_insertar (IN xidVehiculo INT, IN xidConductor INT)
 BEGIN
-	INSERT INTO Vehiculo_has_Conductor (idVehiculo, idConductor)
-	VALUES (xidVehiculo, xidConductor, CURDATE());
+	SELECT estado into @estado_vehiculo
+	from vehiculo
+	where idVehiculo = xidVehiculo;
+    
+	SELECT estado into @estado_Conductor
+	from conductor
+	where idconductor = xidConductor;
+
+	IF(estado_vehiculo = TRUE) THEN
+		IF(estado_Conductor = TRUE) THEN
+			INSERT INTO Vehiculo_has_Conductor (idVehiculo, idConductor)
+			VALUES (xidVehiculo, xidConductor, CURDATE());
+		ELSE
+			SIGNAL SQLSTATE '45000'
+        	SET MESSAGE_TEXT = 'No se puede ACTUALIZAR el Conductor. ESTADO DEL CONDUCTOR = 0';
+    	END IF;
+	ELSE
+    	SIGNAL SQLSTATE '45000'
+    	SET MESSAGE_TEXT = 'No se puede ACTUALIZAR el vehiculo. ESTADO DEL VEHICULO = 0';
+	END IF;
 END $$
 
 /*-----------------------------------------------*/
