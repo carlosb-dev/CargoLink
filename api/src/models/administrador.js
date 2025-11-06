08f5d57e97c937a4b927dfc76eb78c129c7d702a
Francisco Agustin García <132942551+SirFrancis2007@users.noreply.github.com>
2025-11-06 14:22:22 -0300

    creacion de modelos

--- api/src/models/administrador.js
+++ api/src/models/administrador.js
@@ -1,36 +1,39 @@
-// src/models/Empresa.js
+// src/models/Administrador.js
 import { DataTypes } from 'sequelize';
 import sequelize from '../config/database.js';
+import Empresa from './empresa.js';
 
-const Empresa = sequelize.define('Empresa', {
-  idEmpresa: {
+const Administrador = sequelize.define('Administrador', {
+  idAdministrador: {
     type: DataTypes.INTEGER,
     primaryKey: true,
     autoIncrement: true
   },
   Nombre: {
     type: DataTypes.STRING(45),
-    allowNull: false,
-    unique: true
+    allowNull: false
   },
   Contrasena: {
     type: DataTypes.STRING(45),
     allowNull: false
-  },
-  Direccion: {
-    type: DataTypes.STRING(45),
-    allowNull: false,
-    unique: true
   },
   Email: {
     type: DataTypes.STRING(100),
     allowNull: false,
     unique: true,
     validate: { isEmail: true }
+  },
+  idEmpresa: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    references: {
+      model: Empresa,
+      key: 'idEmpresa'
+    }
   }
 }, {
-  tableName: 'Empresa',
+  tableName: 'Administrador',
   timestamps: false
 });
 
-export default Empresa;
+export default Administrador;
--- api/src/models/conductor.js
+++ api/src/models/conductor.js
@@ -1,36 +1,44 @@
-// src/models/Empresa.js
+// src/models/Conductor.js
 import { DataTypes } from 'sequelize';
 import sequelize from '../config/database.js';
+import Empresa from './empresa.js';
 
-const Empresa = sequelize.define('Empresa', {
-  idEmpresa: {
+const Conductor = sequelize.define('Conductor', {
+  idConductor: {
     type: DataTypes.INTEGER,
     primaryKey: true,
     autoIncrement: true
   },
   Nombre: {
     type: DataTypes.STRING(45),
-    allowNull: false,
-    unique: true
-  },
-  Contrasena: {
-    type: DataTypes.STRING(45),
     allowNull: false
   },
-  Direccion: {
+  Licencia: {
     type: DataTypes.STRING(45),
     allowNull: false,
     unique: true
+  },
+  Estado: {
+    type: DataTypes.TINYINT,
+    allowNull: false
   },
   Email: {
     type: DataTypes.STRING(100),
     allowNull: false,
     unique: true,
     validate: { isEmail: true }
+  },
+  idEmpresa: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    references: {
+      model: Empresa,
+      key: 'idEmpresa'
+    }
   }
 }, {
-  tableName: 'Empresa',
+  tableName: 'Conductor',
   timestamps: false
 });
 
-export default Empresa;
+export default Conductor;
