2f4df354f629f1d6dc6255ee7b46f6b0e457de05
Carlos <carlosj.hbello@gmail.com>
2025-11-19 17:39:02 -0300

    delete de vista administrador y conductor

--- cargolink/src/main.tsx
+++ cargolink/src/main.tsx
@@ -12,13 +12,6 @@ import Vehiculos from "./pages/empresa/Vehiculos";
 import Flota from "./pages/empresa/Flota";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import { RUTAS } from "./data/rutas";
-// import EmpresaHistorial from "./pages/empresa/Historial";
-// import ConductorEnvios from "./pages/conductor/Envios";
-// import ConductorPanel from "./pages/conductor/Panel";
-// import Administrador from "./pages/administrador/Panel";
-// import ListaPedidos from "./pages/administrador/ListaPedidos";
-// import ListaOrdenes from "./pages/administrador/ListaOrdenes";
-// import AdministradorHistorial from "./pages/administrador/Historial";
 
 const router = createBrowserRouter([
   { path: RUTAS.HOME, element: <Home /> },
@@ -31,15 +24,6 @@ const router = createBrowserRouter([
   { path: RUTAS.EMPRESA_CONDUCTORES, element: <Conductores /> },
   { path: RUTAS.EMPRESA_VEHICULOS, element: <Vehiculos /> },
   { path: RUTAS.EMPRESA_FLOTA, element: <Flota /> },
-  // { path: RUTAS.EMPRESA_HISTORIAL, element: <EmpresaHistorial /> },
-
-  // { path: RUTAS.CONDUCTOR_PANEL, element: <ConductorPanel /> },
-  // { path: RUTAS.CONDUCTOR_ENVIOS, element: <ConductorEnvios /> },
-
-  // { path: RUTAS.ADMIN_PANEL, element: <Administrador /> },
-  // { path: RUTAS.ADMIN_LISTA_PEDIDOS, element: <ListaPedidos /> },
-  // { path: RUTAS.ADMIN_LISTA_ORDENES, element: <ListaOrdenes /> },
-  // { path: RUTAS.ADMIN_HISTORIAL, element: <AdministradorHistorial /> },
 ]);
 
 createRoot(document.getElementById("root")!).render(
